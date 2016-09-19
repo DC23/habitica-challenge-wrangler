261191c41a7227a69c798f67a766bd1aa42b2d6a
DC23 <jugglindan@gmail.com>
2016-09-19 14:56:48 +0800

    adding metadata

--- habitica_challenge_wrangler/__init__.py
+++ habitica_challenge_wrangler/__init__.py
@@ -3,4 +3,9 @@
 
     Data analysis tool for selecting winners from Habitica challenge data.
 """
+__author__ = 'DC23'
+__email__ = 'jugglindan@gmail.com'
+__version__ = '1.1.0'
+
 from .challenge_wrangler import pick_winner
+
